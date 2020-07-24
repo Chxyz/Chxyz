3ac2bb7932cccdcf550a377e172d138c7e75349e
Chrys <chxyz.apps@gmail.com>
2020-07-24 13:15:22 +0200

    Get started template for HTML, CSS and Javascript

---
+++ Get-Started-HTML-CSS-JS/scripts/scripts.js
@@ -0,0 +1,10 @@
+/* Vanilla Javascript */
+// window.onload = init;
+// function init() {
+//     console.log("DOM ready!");
+// }
+
+/* jQuery - Shorthand for $( document ).ready() */
+// $(function() {
+//     console.log( "DOM ready!" );
+// });
